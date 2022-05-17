a0b751df0761e115a755e24c57c79aaa3ae46b38
Hugo <54809176+HUGOW04@users.noreply.github.com>
2022-05-17 21:16:35 +0200

    Update autoClicker.h

--- autoClicker.h
+++ autoClicker.h
@@ -5,45 +5,51 @@ using namespace std;
 
 class click
 {
-
+int x; 
+int y;
 public:
     void start()
     {
-    int n = 1;
-    int x; 
-    int y;
-    int clicks;
-    int done;
-    int ms;
-
-    cout<<"AutoClicker\n";
-    cout<<"Number of clicks: ";
-    cin>>clicks;
-    cout<<endl;
-    cout<<"Milliseconds between clicks: ";
-    cin>>ms;
-    done = 0;
-
-    cout << "starts in 5..." << endl;
-    Sleep(1000);
-    cout << "starts in 4..." << endl;
-    Sleep(1000);
-    cout << "starts in 3..." << endl;
-    Sleep(1000);
-    cout << "starts in 2..." << endl;
-    Sleep(1000);
-    cout << "starts in 1..." << endl;
-    Sleep(1000);
-
-    while (done <= clicks)
-    {
-        Sleep(ms);
-        mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-        mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
-        done++;
+        SelectClick();     
     }
-    
-    cout << "The clicks have been made." << endl;
-    Sleep(1000);
+
+private:
+    void SelectClick()
+    {
+        int n = 1;
+        
+        int clicks;
+        int done;
+        int ms;
+        cout<<"AutoClicker\n";
+        cout<<"Number of clicks: ";
+        cin>>clicks;
+        cout<<endl;
+        cout<<"Milliseconds between clicks: ";
+        cin>>ms;
+        done = 0;
+
+        cout << "starts in 5..." << endl;
+        Sleep(1000);
+        cout << "starts in 4..." << endl;
+        Sleep(1000);
+        cout << "starts in 3..." << endl;
+        Sleep(1000);
+        cout << "starts in 2..." << endl;
+        Sleep(1000);
+        cout << "starts in 1..." << endl;
+        Sleep(1000);
+
+        while (done <= clicks)
+        {
+            Sleep(ms);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            done++;
+        }
+        
+        cout << "The clicks have been made." << endl;
+        Sleep(1000);
+
     }
 };
